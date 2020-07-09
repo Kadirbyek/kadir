2a5ce0680beea554ea6c43284f26a0c2fffbedc5
Kadirbyek(Kadir) <kadirbek@users.noreply.github.com>
2020-07-09 11:04:36 -0500

    Update maze.js

--- W3D3/maze.js
+++ W3D3/maze.js
@@ -1,11 +1,10 @@
 "use strict";
-
 $(document).ready(function(){
     var start =  $("#start");
     var end = $("#end");
     var maze = $("#maze");
     var boundary = $(".boundary");
-    var gameStart = false;
+    let gameStart = false;
     maze.mouseleave(function(){ loss() });
 
     start.click(function(){
